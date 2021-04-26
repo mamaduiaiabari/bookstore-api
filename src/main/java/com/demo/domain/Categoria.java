f5977f68caf5f0d95a10acbf0a19d6cfa87ef3b3
mamaduiaiabari <mayaba_2005@yahoo.com.br>
2021-04-26 16:59:49 +0000

    finalizando as classes de dominio

--- src/main/java/com/demo/domain/Categoria.java
+++ src/main/java/com/demo/domain/Categoria.java
@@ -1,5 +1,6 @@
 package com.demo.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,14 +11,17 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 @Entity
-public class Categoria {
+public class Categoria implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
 	
-	@OneToMany
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<>();
 
 	public Categoria() {
--- src/main/java/com/demo/domain/Livro.java
+++ src/main/java/com/demo/domain/Livro.java
@@ -1,15 +1,20 @@
 package com.demo.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.ManyToAny;
 
 @Entity
-public class Livro {
+public class Livro implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -18,6 +23,7 @@ public class Livro {
 	private String texto;
 	
 	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
