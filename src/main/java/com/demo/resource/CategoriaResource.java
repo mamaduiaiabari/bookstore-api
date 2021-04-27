6564e66196c9ee821bd21de3f218a6e1b7fe65f3
mamaduiaiabari <mayaba_2005@yahoo.com.br>
2021-04-27 15:10:57 +0000

    findAll para Categoria utilizando padrão DTO

--- src/main/java/com/demo/resource/CategoriaResource.java
+++ src/main/java/com/demo/resource/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.demo.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.domain.Categoria;
+import com.demo.dtos.CategoriaDTO;
 import com.demo.service.CategoriaService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -15,13 +19,20 @@ import javassist.tools.rmi.ObjectNotFoundException;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Categoria> findById(@PathVariable Integer id) throws ObjectNotFoundException{
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id) throws ObjectNotFoundException {
 		Categoria obj = categoriaService.findById(id);
 		return ResponseEntity.ok(obj);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = categoriaService.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDTO);
 	}
 }
