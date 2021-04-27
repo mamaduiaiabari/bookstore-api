08b163b6ec882f62d6de3867159d6660fa7741c0
mamaduiaiabari <mayaba_2005@yahoo.com.br>
2021-04-27 17:44:33 +0000

    UPDATE para categoria

--- src/main/java/com/demo/resource/CategoriaResource.java
+++ src/main/java/com/demo/resource/CategoriaResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -48,5 +49,13 @@ public class CategoriaResource {
 		// retornar para o usuaario a URI do objeto criado
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDTO)
+			throws ObjectNotFoundException {
+		Categoria newObj = categoriaService.update(id, objDTO);
+		return ResponseEntity.ok().body(new CategoriaDTO(newObj));
+
 	}
 }
--- src/main/java/com/demo/service/CategoriaService.java
+++ src/main/java/com/demo/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.demo.domain.Categoria;
+import com.demo.dtos.CategoriaDTO;
 import com.demo.repositories.CategoriaRepository;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -29,6 +30,13 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDTO) throws ObjectNotFoundException {
+		Categoria obj = findById(id);
+		obj.setNome(objDTO.getNome());
+		obj.setDescricao(objDTO.getDescricao());
 		return categoriaRepository.save(obj);
 	}
 }
