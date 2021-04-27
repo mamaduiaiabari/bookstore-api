b5ab23a0f0fed9da4ab92c8472ec74959799afe7
mamaduiaiabari <mayaba_2005@yahoo.com.br>
2021-04-27 09:36:46 +0000

    instancia de forma automatica de base de dados

--- src/main/java/com/demo/BookstoreApplication.java
+++ src/main/java/com/demo/BookstoreApplication.java
@@ -1,13 +1,41 @@
 package com.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.demo.domain.Categoria;
+import com.demo.domain.Livro;
+import com.demo.repositories.CategoriaRepository;
+import com.demo.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner{
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informatica","Livro de TI");
+		
+		Livro l1 = new Livro(null,"Clean Code","Robert Martin","Loren Ipsum",cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+		
 	}
 
 }
