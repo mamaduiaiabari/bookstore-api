04f580e4c0adab505540da81dda6485a8873af35
mamaduiaiabari <mayaba_2005@yahoo.com.br>
2021-04-28 18:09:50 +0000

    lista de todos os Livros por Categoria

--- src/main/java/com/demo/resource/LivroResource.java
+++ src/main/java/com/demo/resource/LivroResource.java
@@ -2,15 +2,18 @@ package com.demo.resource;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.domain.Livro;
+import com.demo.dtos.LivroDTO;
 import com.demo.service.LivroService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -18,13 +21,23 @@ import javassist.tools.rmi.ObjectNotFoundException;
 @RestController
 @RequestMapping(value = "/livros")
 public class LivroResource {
-	
+
 	@Autowired
 	private LivroService livroService;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Livro> findById(@PathVariable Integer id)  throws ObjectNotFoundException{
+	public ResponseEntity<Livro> findById(@PathVariable Integer id) throws ObjectNotFoundException {
 		Livro obj = livroService.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat)
+			throws ObjectNotFoundException {
+		List<Livro> list = livroService.findAll(id_cat);
+		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	
 }
--- src/main/java/com/demo/service/LivroService.java
+++ src/main/java/com/demo/service/LivroService.java
@@ -1,11 +1,13 @@
 package com.demo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.demo.domain.Livro;
+import com.demo.repositories.CategoriaRepository;
 import com.demo.repositories.LivroRepository;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -15,10 +17,18 @@ public class LivroService {
 
 	@Autowired
 	private LivroRepository livroRepository;
+	
+	@Autowired
+	private CategoriaService categoriaService;
 
 	public Livro findById(Integer id)  throws ObjectNotFoundException {
 		Optional<Livro> obj = livroRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Livro não encontrado " + " Id= " + id + " Tipo " + Livro.class.getName()));
+	}
+
+	public List<Livro> findAll(Integer id_cat) throws ObjectNotFoundException {
+		categoriaService.findById(id_cat);
+		return livroRepository.findAllByCategoria(id_cat);
 	}
 }
